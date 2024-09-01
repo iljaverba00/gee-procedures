e0e6f9090792d3e38e59dbeaca1d6ac9ff177ae2
verba <ilja-verba00@yandex.ru>
2024-09-01 21:48:19 +0800

    0.0.7 style

--- src/components/pages/components/AllTabs.vue
+++ src/components/pages/components/AllTabs.vue
@@ -11,7 +11,7 @@ import {
   QItemLabel,
   QTooltip,
   QItemSection,
-  QBtn,
+  QBtn
 } from 'quasar';
 
 import { ref } from 'vue';
@@ -279,13 +279,11 @@ console.log(!props.configProcedure?.onlyFavorites && !props.onlyImport);
   </q-tab-panels>
 </template>
 
-<style scoped>
-:deep(.q-tree__tickbox) {
-  margin-right: 4px;
+<style lang="sass">
+:deep(.q-tree__tickbox)
+  margin-right: 4px
   display: none !important
-}
 
-.tab-panels {
+.tab-panels
   height: calc(100% - 55px)
-}
 </style>
